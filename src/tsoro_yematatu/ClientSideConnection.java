d3876ed290e53f5481216a491248e2940ca07c9d
Matheus Holanda Matos <matheus.holandam@gmail.com>
2022-02-24 16:23:18 -0300

    Refactoring code

---
+++ src/tsoro_yematatu/ClientSideConnection.java
@@ -0,0 +1,122 @@
+package tsoro_yematatu;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+
+public class ClientSideConnection {
+	
+	private int playerID;
+
+	private Socket socket;
+	private DataInputStream dataIn;
+	private DataOutputStream dataOut;
+	
+	public ClientSideConnection() {
+		System.out.println("----Client----");
+		
+		try {
+			socket = new Socket("localhost", 51734);
+			dataIn = new DataInputStream(socket.getInputStream());
+			dataOut = new DataOutputStream(socket.getOutputStream());
+
+			setPlayerID(dataIn.readInt());
+			System.out.println("Connected to server as Player #" + playerID + ".");
+		}
+		catch (IOException e) {
+			System.out.println("IOException - ClientSideConnection()");
+		}
+	}
+	
+	public void sendButtonNum (int n) {
+		try {
+			dataOut.writeInt(n);
+			dataOut.flush();
+		}
+		catch (IOException e) {
+			System.out.println("IO Exception - sendButtonNum()");
+		}
+	}
+	
+	public int receiveButtonNum () {
+		int n = -1;
+		
+		try {
+			n = dataIn.readInt();
+		}
+		catch (IOException e) {
+			System.out.println("IO Exception - receiveButtonNum()");
+		}
+		
+		return n;
+	}		
+	
+	public void sendUpdatedPoints (int n0, int n1, int n2) {
+		try {
+			dataOut.writeInt(n0);
+			dataOut.writeInt(n1);
+			dataOut.writeInt(n2);
+			dataOut.flush();
+		}
+		catch (IOException e) {
+			System.out.println("IO Exception - sendUpdatedPoints()");
+		}
+	}
+	
+	public Integer[] receiveUpdatedPoints () {
+		Integer[] n = {-1, -1, -1};
+		
+		try {
+			n[0] = dataIn.readInt();
+			n[1] = dataIn.readInt();
+			n[2] = dataIn.readInt();
+		}
+		catch (IOException e) {
+			System.out.println("IO Exception - receiveUpdatedPoints()");
+		}
+		
+		return n;
+	}
+	
+	public void sendUpdateArrayFlag (boolean updateArray) {
+		try {
+			dataOut.writeBoolean(updateArray);
+			dataOut.flush();
+		}
+		catch (IOException e) {
+			System.out.println("IO Exception - sendUpdateArrayFlag()");
+		}
+	}
+	
+	public boolean receiveUpdateArrayFlag  () {
+		boolean updateArray = false;
+		
+		try {
+			updateArray = dataIn.readBoolean();
+		}
+		catch (IOException e) {
+			System.out.println("IO Exception - receiveUpdateArrayFlag()");
+		}
+		
+		return updateArray;
+	}
+
+	public void closeConnection() {
+		try {
+			socket.close();
+			System.out.println("----CONNECTION CLOSED----");
+		}
+		catch (IOException e) {
+			System.out.println("IOException - closeConnection()");
+		}
+	}
+	
+	public int getPlayerID() {
+		return playerID;
+	}
+
+	public void setPlayerID(int playerID) {
+		this.playerID = playerID;
+	}
+}
--- src/tsoro_yematatu/Player.java
+++ src/tsoro_yematatu/Player.java
@@ -4,10 +4,6 @@ import java.awt.Container;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
-import java.io.IOException;
-import java.net.Socket;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -26,7 +22,6 @@ public class Player extends JFrame {
 	private JTextArea message;
 	private JButton[] buttons = new JButton[7];
 	
-	private int playerID;
 	private int otherPlayer;
 	
 	private int turnsMade;
@@ -73,7 +68,7 @@ public class Player extends JFrame {
 	
 	public void setUpGUI() {
 		this.setSize(width, height);
-		this.setTitle("Player #" + playerID);
+		this.setTitle("Player #" + clientSideConnection.getPlayerID());
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
 		contentPane.setLayout(new GridLayout(1, 8));
@@ -92,7 +87,7 @@ public class Player extends JFrame {
 		contentPane.add(buttons[5]);
 		contentPane.add(buttons[6]);
 		
-		if (playerID == 1) {
+		if (clientSideConnection.getPlayerID() == 1) {
 			message.setText("You are player #1. You go first.");
 			otherPlayer = 2;
 			buttonsEnabled = true;
@@ -309,112 +304,6 @@ public class Player extends JFrame {
 		segments.add(vertical);
 		
 		return segments;
-	}
-	
-	// Client Connection Inner Class
-	private class ClientSideConnection {
-		private Socket socket;
-		private DataInputStream dataIn;
-		private DataOutputStream dataOut;
-		
-		public ClientSideConnection() {
-			System.out.println("----Client----");
-			
-			try {
-				socket = new Socket("localhost", 51734);
-				dataIn = new DataInputStream(socket.getInputStream());
-				dataOut = new DataOutputStream(socket.getOutputStream());
-
-				playerID = dataIn.readInt();
-				System.out.println("Connected to server as Player #" + playerID + ".");
-			}
-			catch (IOException e) {
-				System.out.println("IOException - ClientSideConnection()");
-			}
-		}
-		
-		public void sendButtonNum (int n) {
-			try {
-				dataOut.writeInt(n);
-				dataOut.flush();
-			}
-			catch (IOException e) {
-				System.out.println("IO Exception - sendButtonNum()");
-			}
-		}
-		
-		public int receiveButtonNum () {
-			int n = -1;
-			
-			try {
-				n = dataIn.readInt();
-			}
-			catch (IOException e) {
-				System.out.println("IO Exception - receiveButtonNum()");
-			}
-			
-			return n;
-		}		
-		
-		public void sendUpdatedPoints (int n0, int n1, int n2) {
-			try {
-				dataOut.writeInt(n0);
-				dataOut.writeInt(n1);
-				dataOut.writeInt(n2);
-				dataOut.flush();
-			}
-			catch (IOException e) {
-				System.out.println("IO Exception - sendUpdatedPoints()");
-			}
-		}
-		
-		public Integer[] receiveUpdatedPoints () {
-			Integer[] n = {-1, -1, -1};
-			
-			try {
-				n[0] = dataIn.readInt();
-				n[1] = dataIn.readInt();
-				n[2] = dataIn.readInt();
-			}
-			catch (IOException e) {
-				System.out.println("IO Exception - receiveUpdatedPoints()");
-			}
-			
-			return n;
-		}
-		
-		public void sendUpdateArrayFlag (boolean updateArray) {
-			try {
-				dataOut.writeBoolean(updateArray);
-				dataOut.flush();
-			}
-			catch (IOException e) {
-				System.out.println("IO Exception - sendUpdateArrayFlag()");
-			}
-		}
-		
-		public boolean receiveUpdateArrayFlag  () {
-			boolean updateArray = false;
-			
-			try {
-				updateArray = dataIn.readBoolean();
-			}
-			catch (IOException e) {
-				System.out.println("IO Exception - receiveUpdateArrayFlag()");
-			}
-			
-			return updateArray;
-		}
-	
-		public void closeConnection() {
-			try {
-				socket.close();
-				System.out.println("----CONNECTION CLOSED----");
-			}
-			catch (IOException e) {
-				System.out.println("IOException - closeConnection()");
-			}
-		}
 	}
 	
 	public static void main(String[] args) {
